b49fc5caff546feed49ac239a232db3a633d62b2
Fowl3r <tcd.fowler@gmail.com>
2022-12-18 22:44:44 +0000

    cookies and sessions covered

--- app.js
+++ app.js
@@ -1,44 +1,82 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+// https://www.passportjs.org/howtos/password/ docs for passport
+const passport = require("passport");
+// nodeJS crypto library
+const crypto = require("crypto");
+const routes = require("./routes");
+const connection = require("./config/database");
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
 
 // Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
+// takes passport.use(stratergy) from passport.js and includes it in app.js
+require("./config/passport");
 
 /**
  * -------------- GENERAL SETUP ----------------
  */
 
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
+require("dotenv").config();
 
 // Create the Express application
 var app = express();
 
+// middleware for parsing http responses
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * -------------- SESSION SETUP ----------------
  */
 
-// TODO
+/* 
+Express session gives access to the request.session object
+ and anything that is stored on this object
+in any of the routes will be persisted to the database under the sessions collection.
+Knowing this passport recognises this and uses it as a user authentication 
+*/
+
+const sessionStore = new MongoStore({
+	mongooseConnection: connection,
+	collection: "session",
+});
+
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: true,
+		store: sessionStore,
+		cookie: {
+			// Math for a day
+			maxAge: 1000 * 60 * 60 * 24,
+		},
+	})
+);
 
 /**
  * -------------- PASSPORT AUTHENTICATION ----------------
  */
-
+require("./config/passport");
+/* Every time route is loaded these 2 middlewares work together
+check if passport.user is != to null, then knows there is a logged in user
+grabs the user id from passport.user property then uses deserialiseUser method pass in user ID and grab that from database, then populate req.user to what was grabbed from the db - happens every route request */
+// initialiase & refresh passport middleware for each route
 app.use(passport.initialize());
+// express session middleware
 app.use(passport.session());
 
+app.use((req, res, next) => {
+	// express session creates this object
+	console.log(req.session);
+	// passport middleware creates this object
+	console.log(req.user);
+	next();
+});
 
 /**
  * -------------- ROUTES ----------------
@@ -46,7 +84,6 @@ app.use(passport.session());
 
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
-
 
 /**
  * -------------- SERVER ----------------
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,79 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
+/* 
+Did not work when storing verifyCallback as a const with anon function,
+believe this was done to allow for customFields - but these did not work either.
+*/
+
+const passport = require("passport");
+const validPassword = require("../lib/passwordUtils").validPassword;
+const LocalStrategy = require("passport-local").Strategy;
+const connection = require("./database");
+// Defined in database, User model Schema
 const User = connection.models.User;
 
-// TODO: passport.use();
+// define in customFields object so passportJS knows where to look
+const customFields = {
+	usernameField: "uname",
+	passwordField: "pw",
+};
+
+/* 
+
+What happens is we make a post request and we provide a JSON body with 'uname' & 'pw'
+then passport as a middleware will look for these 2 fields then populate the parameters username & password with the value from the usernameField and passwordField
+*/
+
+/* must name username & password exactly these variables so that Passport local strategy
+ can detect them, unless using custom Fields Object 
+Here we are basically doing our own implementation of verification.
+doesn't matter what db or method of verifying credentials
+all that matters is the return values that are passed to done (callback) are what passport expects
+*/
+function verifyCallback(username, password, done) {
+	// Below we are searching the mongoDB database 'User' and looking for a username that matches the one provided in the verifyCallback parameter
+	User.findOne({ username: username })
+		// user is returned in a basic promise
+		.then(user => {
+			console.log(user);
+			// if no user in db , letting passport know that there isn't an error but there is no user matching in db
+			if (!user) {
+				return done(null, false);
+			}
+
+			// check if password is valid, putting password thorugh verification function and verifying hash and salt stored in db against the password
+			const isValid = validPassword(password, user.hash, user.salt);
+			// if credentials are valid return callback
+			if (isValid) {
+				// returns no error and user that has been verified, let us into route
+				return done(null, user);
+			} else {
+				// no error but user not verified so do not let into route
+				return done(null, false);
+			}
+		})
+		// catch any errors that happen on express app or db, pass it to passport
+		.catch(err => {
+			done(err);
+		});
+}
+
+// create new basic stratergy
+const strategy = new LocalStrategy(verifyCallback);
+
+// Pass in strategy below
+passport.use(strategy);
+
+// used for express session, this function when we use passport.authenticate()
+// grabs user from db, gets id of it then inserts it into req.session.passport.user property
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+// When we need to grab the user from the session we use this function
+passport.deserializeUser((userId, done) => {
+	// grabs user from database, then attatches found user to req.user object
+	User.findById(userId)
+		.then(user => {
+			done(null, user);
+		})
+		.catch(err => done(err));
+});
